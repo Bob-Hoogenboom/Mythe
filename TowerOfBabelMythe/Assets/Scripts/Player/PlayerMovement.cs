d7d3c49aad9220c8b68e56adfd5dd948c0c3e590
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-05-19 00:40:27 +0200

    Feature_Movement.Update
    Added a new scene. and a place holder character with movement script.

---
+++ TowerOfBabelMythe/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 8f;
+    [SerializeField] Vector3 _direction;
+
+    private CharacterController _controller;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
