1c695cccd85032c6d590fbeaf5d4f761b9c8cb4d
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-05-20 00:41:29 +0200

    Feature_Movement.Prototype
    movement prototype start

---
+++ TowerOfBabelMythe/Assets/Scripts/Player/KeyboardInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour
+{
+    
+    
+    void Update()
+    {
+        //right
+        if (Input.GetKey(KeyCode.D))
+        {
+            VirtualInputManager.Instance._MoveRight = true;
+        }
+        else
+        {
+            VirtualInputManager.Instance._MoveRight = false;
+        }
+
+        //left
+        if (Input.GetKey(KeyCode.A))
+        {
+            VirtualInputManager.Instance._MoveLeft = true;
+        }
+        else
+        {
+            VirtualInputManager.Instance._MoveLeft = false;
+        }
+    }
+}
---
+++ TowerOfBabelMythe/Assets/Scripts/Player/VirtualInputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualInputManager : MonoBehaviour
+{
+    public static VirtualInputManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public bool _MoveRight;
+    public bool _MoveLeft; 
+
+}
