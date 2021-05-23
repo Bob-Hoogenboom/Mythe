8ea51205a3bf37026700344dc68875bdca4cf4ad
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-05-23 23:52:30 +0200

    Feature_Movement.Update.2
    deleted singleton movement and added a more simple movement system.

---
+++ TowerOfBabelMythe/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _Speed;
+
+
+    
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * _Speed;
+
+        if (!Mathf.Approximately(0, movement))
+            transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+    }
+}
