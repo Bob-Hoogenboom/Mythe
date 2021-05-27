761b59e28e8b417c00281d6742d46dfc9bb8bc4c
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-05-27 16:02:57 +0200

    Feature_Movement.Update.4
    K.I.S.S.
    RigidBody movement

---
+++ TowerOfBabelMythe/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float _speed = 5f;
+
+    [SerializeField] Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        _rb.velocity = new Vector3(moveHorizontal, _rb.velocity.y, 0) * _speed;
+
+        
+    }
+}
