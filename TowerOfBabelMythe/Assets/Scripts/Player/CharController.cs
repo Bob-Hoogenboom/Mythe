9f56ddb9376c0dd1bd443b172f2274be2273fa96
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-05-28 23:04:42 +0200

    Feature_Movement.Update.6
    finally settled on movement and finished it. ready for animation and prototyping.

---
+++ TowerOfBabelMythe/Assets/Scripts/Player/CharController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    public Animator anime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anime.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        anime.SetFloat("vertical", Input.GetAxis("Vertical"));
+    }
+}
