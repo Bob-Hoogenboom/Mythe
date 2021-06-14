48ffbd3a44391fd6e7a31b417b4ee1b71cdff290
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-06-14 11:07:15 +0200

    Developer.Update.8
    small tweaks and changes.

---
+++ TowerOfBabelMythe/Assets/Scripts/Player/RotaryMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotaryMovement : MonoBehaviour
+{
+    [SerializeField] GameObject _target;
+    [SerializeField] float _angle = 20f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.RotateAround(_target.transform.position, Vector3.up, _angle * Time.deltaTime);
+    }
+}
