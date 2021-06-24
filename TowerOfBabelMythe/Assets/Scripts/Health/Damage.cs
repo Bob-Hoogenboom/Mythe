c5eeb1534cd016b65457673222dc95fcb4b945e5
Bob Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-06-24 10:21:53 +0200

    Feature_Movement.Update.15
    assets tweaked

---
+++ TowerOfBabelMythe/Assets/Scripts/Health/Damage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public float damagePoints = 1f;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Damage Dealth");
+
+
+        Health H = other.GetComponent<Health>();
+
+        H.healthPoints -= damagePoints;
+
+    }
+}
---
+++ TowerOfBabelMythe/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float startHealth = 100f;
+    public float healthPoints
+    {
+        get { return _hP; }
+        set
+        {
+            _hP = Mathf.Clamp(value,0f,100f);
+
+            if(_hP <= 0f)
+            {
+                Death();
+            }
+        }
+    }
+    [SerializeField] float _hP = 100f;
+
+    void Start()
+    {
+        healthPoints = startHealth;
+    }
+
+    void Death()
+    {
+        Destroy (gameObject);
+    }
+
+}
